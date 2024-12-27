4204fe5a168b93ea6dddf6e58b802419aa89be81
aya-abounacer <ayaabounacer1@gmail.com>
2024-12-27 15:07:10 +0100

    first commit

---
+++ app/Views/exams.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+<div class="container">
+    <h1>Gestion des Exams Universitaire</h1>
+    <p>Welcome to the Exams Management System.</p>
+</div>
+<?= $this->endSection() ?>
